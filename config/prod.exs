5835443673bab6b5a1f1237eaaaaf21f0109ed01
Connor Smallman <connor.smallman@bedegaming.com>
2016-12-28 15:49:08 +0000

    Removed trailing comma prod config

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ config :sopost_people, SopostPeople.Endpoint,
   url: [scheme: "https", host: " guarded-taiga-21088.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
